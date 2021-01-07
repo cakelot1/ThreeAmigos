6c7fba6b86d7b1c871329067493bf2b39ce92f5f
cakelot1 <v8073836@live.tees.ac.uk>
2021-01-07 12:57:27 +0000

    Changes

--- ThAmCo.Catering/Program.cs
+++ ThAmCo.Catering/Program.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace ThAmCo.Catering
@@ -14,7 +16,19 @@ namespace ThAmCo.Catering
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope()) {
+                var sevices = scope.ServiceProvider;
+                var env = sevices.GetRequiredService<IHostingEnvironment>();
+                if (env.IsDevelopment()) {
+                    var context = sevices.GetRequiredService<CateringContext>();
+                    context.Database.EnsureDeleted();
+                    context.Database.Migrate();
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- ThAmCo.Catering/Startup.cs
+++ ThAmCo.Catering/Startup.cs
@@ -25,6 +25,7 @@ namespace ThAmCo.Catering
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddDbContext<CateringContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
